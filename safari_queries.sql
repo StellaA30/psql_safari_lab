3b51cdbec9701077db4816e4c24ccff6c231451e
Stella Annor <stella.annor@icloud.com>
2023-04-14 14:37:52 +0100

    adding solution to MVP question 2

--- safari_queries.sql
+++ safari_queries.sql
@@ -4,3 +4,21 @@
 SELECT name 
 FROM animals
 WHERE enclosure_id = 1;
+
+-- 2. The names of the staff working in a given enclosure
+-- SELECT employee_id, enclosure_id
+-- FROM assignments
+-- WHERE enclosure_id = 1;
+
+
+-- SELECT name
+-- FROM employees
+-- WHERE id = 2 OR id = 1;
+
+SELECT name
+FROM employees
+WHERE id IN (SELECT employee_id
+FROM assignments
+WHERE enclosure_id = 1);
+
+
