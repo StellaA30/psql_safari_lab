5a946b01846d829467be394a13b7f1a1e1d2994f
Stella Annor <stella.annor@icloud.com>
2023-04-14 15:03:19 +0100

    removed comments

--- safari_queries.sql
+++ safari_queries.sql
@@ -6,14 +6,6 @@ FROM animals
 WHERE enclosure_id = 1;
 
 -- 2. The names of the staff working in a given enclosure
--- SELECT employee_id, enclosure_id
--- FROM assignments
--- WHERE enclosure_id = 1;
-
-
--- SELECT name
--- FROM employees
--- WHERE id = 2 OR id = 1;
 
 SELECT name
 FROM employees
@@ -22,9 +14,6 @@ FROM assignments
 WHERE enclosure_id = 1);
 
 -- 3. the names of staff working in enclosures which are closed for maintenance
--- SELECT name, id
--- FROM enclosures
--- WHERE closed_for_maintenance = true;
 
 SELECT name 
 FROM employees
