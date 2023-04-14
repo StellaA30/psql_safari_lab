c9ea48c7864c1f984cd1b49d0fe4775f9351baa6
maryan axmed <12maxmed@gmail.com>
2023-04-14 15:00:13 +0100

    second solution to extension task

--- safari_queries.sql
+++ safari_queries.sql
@@ -21,4 +21,17 @@ WHERE id IN (SELECT employee_id
 FROM assignments
 WHERE enclosure_id = 1);
 
+-- 3. the names of staff working in enclosures which are closed for maintenance
+SELECT name, id
+FROM enclosures
+WHERE closed_for_maintenance = true;
+
+SELECT name 
+FROM employees
+WHERE id IN (
+    SELECT employee_id
+    FROM assignments
+    WHERE enclosure_id = 3)
+;
+
 
